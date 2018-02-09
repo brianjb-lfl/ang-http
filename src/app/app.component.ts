7f43a4349360f8a1c6e6ee547abb30a1bf803611
Brian Besaw <bjbesaw@outlook.com>
2018-02-08 23:30:08 -0500

    Post with auth info in headers template working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient) { }
+
+  onLogin() {
+    const b64Encoded = btoa(`${'demovoter'}:${'demovoterpw'}`);
+    const loginUrl = 'http://localhost:8080/api/auth/login';
+
+    const headers = new HttpHeaders({'Authorization':'Basic ' + b64Encoded});
+    console.log(headers);
+
+    return this.http.post(loginUrl, { headers })
+      .subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      );
+
+  }
+
+
 }
